94b398751ceb7f4cad5893cfb11e79776ed4527b
JeronimoGomez <jeronimo.gomez-duque@cpe.fr>
2025-10-07 08:32:27 +0200

    Add files via upload

---
+++ Jeu_casse_briques.py
@@ -0,0 +1,14 @@
+#Jeronimo Gomez
+#Daoud Hechaichi
+
+#7/10/2023
+
+#But: créer le jeu du casse briques, en utilisant tkinter et programation orientée objet
+
+From tkinter import tk, canvas,
+
+fenetre = tk.Tk()
+fenetre.title("Casse Briques")
+
+
+
